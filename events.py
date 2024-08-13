f651188eb6585cab301183df6cf39679b943d227
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-08-13 00:09:30 -0300

    discord webhook

--- events.py
+++ events.py
@@ -3,7 +3,7 @@ import datetime
 import requests
 
 client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
-webhook_url = "https://discord.com/api/webhooks/960648103646613544/gaZzoaUqfbanScFdZfZdQ1QvY1gutMAuyfJBO0CxcHQEviUFHhAtSrlQ6ukwYM758WlY"
+webhook_url = "https://discord.com/api/webhooks/1272715325087023206/hyZo7uJESOjdHbaoXYNU2-hmLx315cBk3k9skEjm-G9s0Ps2O3-smsoUFE75lkb5Nkho"
 
 for event in client.events(decode=True, filters={"event": "die"}):
   container_id = event["id"]
@@ -11,7 +11,7 @@ for event in client.events(decode=True, filters={"event": "die"}):
   epoch_time = event["time"]
   date_time = datetime.datetime.fromtimestamp(epoch_time)
 
-  payload = {"content": "O container %s (%s) foi finalizado às %s" % (container_name, container_id, date_time)}
+  payload = {"content": "The container %s (%s) terminated at %s" % (container_name, container_id, date_time)}
 
   print (payload)
   requests.post(webhook_url, data=payload)
